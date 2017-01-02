5dfa55c59e5d2f90f9dd1fafdc910a146d9d05f9
casperpanduro <b0:74:e2:53:a7:d2:c8:a7:95:ba:a9:22:41:dc:28:cbb0:74:e2:53:a7:d2:c8:a7:95:ba:a9:22:41:dc:28:cbcasperj@me.com>
2017-01-02 08:37:17 +0100

    First real commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Takeitover</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+</head>
+<body>
+
+	<section>
+		<div class="container">
+			<div id="takeitover">
+				<div class="takeitover-trigger" data-target="#second">
+					<p>TEST</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TEST</p>
+				</div>
+				<div class="takeitover-content" id="second">
+					<div class="container">
+						<p>2. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+				<div class="takeitover-trigger">
+					<p>TESTs</p>
+				</div>
+				<div class="takeitover-content">
+					<div class="container">
+						<p>3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<script src="takeitover.js"></script>
+	<script>
+		$(document).ready(function(){
+			$("#takeitover").takeitover({
+				complete: function(){
+					console.log("DONW");
+				}
+			});
+		});
+	</script>
+</body>
+</html>
---
+++ takeitover.js
@@ -0,0 +1,214 @@
+
+// Developed by Casper Panduro
+
+(function ( $ ) {
+    $.fn.takeitover = function( options ) {
+        $("body.takeitover-noscroll").css({
+            "overflow-y": "hidden",
+            "height": "100%"
+        });
+        // This is the easiest way to have default options.
+        var settings = $.extend({
+            // These are the defaults.
+            speed: 500,
+            easing: false,
+            elementSelector: '.takeitover-trigger',
+            contentSelector: '.takeitover-content',
+            background: 'rgba(255,255,255,0.95)',
+            clickOnOverlay: true,
+            closeButton: true,
+            buttonColor: "black",
+            complete: null
+        }, options );
+        
+
+        // css to elements
+        $(settings.elementSelector).css({
+            "cursor":"pointer"
+        });
+
+        // hide content
+        $(settings.contentSelector).hide().css({
+            "position":"fixed",
+            "top":"100px",
+            "left":"0px",
+            "width":"100%",
+            "height":"100%",
+            "overflow-y":"scroll",
+            "z-index": 999999,
+            "opacity":0,
+            "-webkit-overflow-scrolling": "touch"
+        });
+
+        // Append overlay
+        $("body").append('<div class="takeitover-overlay" style="background: '+settings.background+'"></div>');
+
+        // overlay css
+        $(".takeitover-overlay").css({
+            "position": "fixed",
+            "top": "0px",
+            "right": "0px",
+            "bottom": "0px",
+            "left": "0px",
+            "display": "none",
+            "opacity": "0",
+            "z-index": "99999"
+        });
+
+        // closebtn
+        if(settings.closeButton == true) {
+            $("body").append('<div class="takeitover-closebtn"><span class="first"></span><span class="last"></span></div>');
+
+            $(".takeitover-closebtn").css({
+                "position":"fixed",
+                "top":"30px",
+                "right":"30px",
+                "z-index":9999999,
+                "width":30,
+                "height":30,
+                "opacity":0,
+                "cursor":"pointer"
+            }).hide();
+
+            $(".takeitover-closebtn span").eq(0).css({
+                "position":"absolute",
+                "top":"50%",
+                "left":"0px",
+                "height":"2px",
+                "width":"100%",
+                "display":"block",
+                "background":settings.buttonColor,
+                "transform":"translateY(-50%) rotate(-45deg)",
+                "-webkit-transform":"translateY(-50%) rotate(-45deg)",
+                "-ms-transform":"translateY(-50%) rotate(-45deg)"
+            });
+
+            $(".takeitover-closebtn span").eq(1).css({
+                "position":"absolute",
+                "top":"50%",
+                "left":"0px",
+                "height":"2px",
+                "width":"100%",
+                "display":"block",
+                "background":settings.buttonColor,
+                "transform":"translateY(-50%) rotate(45deg)",
+                "-webkit-transform":"translateY(-50%) rotate(45deg)",
+                "-ms-transform":"translateY(-50%) rotate(45deg)"
+            });
+        }
+        var easing;
+        if(settings.easing == false) {
+            easing = "";
+        }
+        else {
+            if (jQuery.ui) {
+              easing = settings.easing;
+            }
+            else {
+                easing = "";
+                console.log("takeitover.js: JQUERY UI needed for easing options");
+            }
+        }
+
+        // easing
+        var id = settings.contentSelector;
+        console.log("hl: "+settings.contentSelector);
+
+        // activating takeitover
+        $(document).on("click", settings.elementSelector, function(){
+            var $element = $(this);
+            var target = $element.attr("data-target");
+            if(target == null) {
+                target = settings.contentSelector;
+                target = $element.next(target);
+            }
+            else {
+                target = $(target);
+            }
+            console.log(target);
+            $("body").css({
+                "overflow-y": "hidden",
+                "height": "100%"
+            });
+            $(".takeitover-overlay").show().animate({
+                "opacity":1
+            }, settings.speed);
+
+            setTimeout(function(){
+                if(settings.closeButton == true) {
+                    $(".takeitover-closebtn").show().animate({
+                        "opacity":1
+                    }, settings.speed/2);
+                };
+                target.show();
+                target.animate({
+                    "top":"0px",
+                    "opacity":1
+                }, settings.speed/2, easing, function(){
+                    if ( $.isFunction( settings.complete ) ) {
+                        settings.complete.call( this );
+                    }
+                });
+                
+            }, settings.speed/2);
+        });
+
+        // close options
+        var closeSelector;
+
+        function closeTakeitover() {
+            var $element = $(settings.contentSelector);
+            $element.animate({
+                "top":"100px",
+                "opacity":0
+            }, settings.speed/2, function(){
+                $(this).hide();
+            });
+            $(".takeitover-closebtn").animate({
+                "opacity":0
+            }, settings.speed/2, function(){
+                $(this).hide();
+            });
+
+            setTimeout(function(){
+                $("body").css({
+                    "overflow-y": "scroll",
+                    "height": "auto"
+                });
+                $element.next(settings.contentSelector).show();
+                $(".takeitover-overlay").animate({
+                    "opacity":0
+                }, settings.speed, function(){
+                    $(this).hide();
+                });
+            }, settings.speed/2);
+        };
+
+        if(settings.clickOnOverlay == true) {
+            // close when clicking overlay
+            closeSelector = settings.contentSelector;
+            $(closeSelector).click(function(e){
+                if (e.target !== this) {
+                    
+                }
+                else {
+                    closeTakeitover();
+                }
+                return;
+
+            });
+        }   
+        if(settings.closeButton == true) {
+            $(document).on("click", ".takeitover-closebtn", function(){
+                closeTakeitover();
+            });
+        }
+        // closing document by clicking on overlay
+        
+        return this;
+    };
+ 
+}( jQuery ));
+
+
+
